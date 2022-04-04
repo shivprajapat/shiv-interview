ce62cb301eab01890231d5e8209135c7b9858218
shivprajapat <shivkumarprajapat232@gmail.com>
2022-04-04 18:01:35 +0530

    React router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import EditorPage from './pages/EditorPage';
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route
+            path="/editor/:roomId"
+            element={<EditorPage />}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
